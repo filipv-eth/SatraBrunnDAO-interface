c6e9707889aa72f0f8d6d230f071b2ef4658334a
Evie <evita@stenqvist.co>
2021-12-31 12:44:33 +0100

    Header :white_check_mark:

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,33 @@
 <script>
-	export let name;
+  import Header from "./Header.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Header />
+<main />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :global(body) {
+    /* The light background color
+		Putting it here, instead of global.css, as we will dynamically want to switch between dark and light
+		*/
+    background: linear-gradient(
+        0.47deg,
+        rgba(0, 136, 255, 0.2) 0.39%,
+        rgba(220, 230, 239, 0.2) 99.58%
+      ),
+      linear-gradient(0deg, #dce6ef, #dce6ef);
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+  <img src="logo.svg" alt="logo" />
+  <h1>Sätra Brunn DAO</h1>
+  <!-- Should probably just be a svg rectangle, but this is quick -->
+  <img src="rectangle-palette.png" alt="color palette" />
+</header>
+
+<style>
+    header {
+        display: flex;
+        align-items: center;
+    }
+    img {
+        margin: 1rem;
+    }
+
+    img:last-of-type {
+        margin-left: auto;
+        width: 250px;
+    }
+</style>
