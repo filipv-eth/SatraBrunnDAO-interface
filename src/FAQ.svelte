6fa4007436c0567d4481597ba7d7a16cc801f467
Evie <evita@stenqvist.co>
2022-01-01 15:11:46 +0100

    :construction: FAQ components

---
+++ src/FAQ.svelte
@@ -0,0 +1,26 @@
+<script>
+	import CollapsibleSection from './CollapsibleSection.svelte'
+</script>
+
+<section>
+<CollapsibleSection headerText={'Collapse or Expand me'} >
+	<div class="content">
+			Look at all this fun content
+	</div>
+</CollapsibleSection>
+	
+<CollapsibleSection headerText={'Collapse or Expand me'} >
+	Look at all this fun content
+</CollapsibleSection>
+</section>
+
+<style>
+	section {
+		width: 500px;
+	}
+	
+	.content {
+		background-color: #f4f4f4;
+		padding: 0.5em;
+	}
+</style>
